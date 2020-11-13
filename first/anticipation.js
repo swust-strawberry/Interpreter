b3f8427e376120b4b345c3badd03c4a0607ed843
Yinha <466880466@qq.com>
2020-11-13 23:41:19 +0800

    first com

--- first/anticipation.js
+++ first/anticipation.js
@@ -1,56 +1,75 @@
 class Anticipation{
-    static get_code(str){
-        for(let i=0;i<str.length;i++)
-        {
-            for()
-        }
-    }
-    static separate(str){
-        str = this.clear_note(str);
-        for(let i=0;i<str.length;i++){
-            Array[i] = this.clear_note(str[i]);
-
-        }
-        let w = Array;
-        let flag1=0,flag2=0,flag3=0;
-        str=str.trim();
-        for(let j=0;j<str.length;j++){
-            if(str.charAt(j)===':'){
-                flag1=1;
-                str=str.replace(":"," ");
-            }else if(str.charAt(j)===';'){
-                flag2=j;
-                str=str.replace(";"," ");
-            }else if(str.charAt(j)===','){
-                flag3++;
-                str=str.replace(","," ");
-            }
-        }
-        let x = [];
-        x = flag2!==0 ? str.slice(0,flag2) : str;
-        x = x.split(" ");
-        let n=0;
-        let y = [];
-        for(let j=0;j<x.length;j++) {
-            if (x[j] !== "") {
-                y[n] = x[j];
-                n++;
-            }
-        }
-        return flag1 ? y.slice(1, n) : y;
-    }
-
-    static clear_note(str){    //清除注释
+    static getSegment(str,segmentName){     //得到指定段的内容
+        str = str.separate_wholeCode(str);
         for(let i=0;i<str.length;i++) {
-            for(let j=0;j<str[i].length;j++){
-                str[i] = str[i][j]===';' ? str[i].slice(0,j) : str[i];
+            if(str[i][0] === segmentName && str[i][1] === "SEGMENT"){
+                for(let j=i+1;j<str.length;j++){
+                    if(str[j][0] === segmentName && str[j][1] === "ENDS"){
+                        return str.slice(i+2,j);
+                    }
+                }
+            }
+        }
+    }
+
+    static getSegmentName(str,regName){    //得到指定段对应的段名
+        str = str.separate_wholeCode(str);
+        for(let i=0;i<str.length;i++){
+            if(str[i][0] === "ASSUME"){
+                if(str[i][1].slice(0,2)===regName)
+                    return str[i][2];
+                else if(str[i][3].slice(0,2)===regName)
+                    return str[i][4];
+                else if(str[i][5].slice(0,2)===regName)
+                    return str[i][6]
+            }
+        }
+    }
+
+    static separate_singleCode(str){    //单行代码的单词分割
+        str = this.clear_note(str);
+        let colon_flag = 0,comma_flag = 0;
+        str = str.trim();
+        for(let i=0;i<str.length;i++){
+            if(str.charAt(i)===':'){
+                str = this.insertStr(str,i+1,' ');
+            }
+        }
+        str = str.replace(","," ");
+        str = str.split(" ");
+        let len=0;
+        let endStr = Array();
+        for(let i=0;i<str.length;i++) {
+            if (str[i] !== "") {
+                endStr[len] = str[i];
+                len++;
+            }
+        }
+        return endStr;
+    }
+
+    static separate_wholeCode(str){      //整行代码的单词分割
+        let endStr = Array(),len=0;
+        for(let i=0;i<str.length;i++){
+            if(this.separate_singleCode(str[i]).length === 0) continue;
+            endStr[len]=this.separate_singleCode(str[i]);
+            len++;
+        }
+        return endStr;
+    }
+
+    static clear_note(str){    //清除单行代码的注释
+        for(let i=0;i<str.length;i++) {
+            if(str[i]===';'){
+                str = str.slice(0,i);  break;
             }
         }
         return str;
     }
-}
-let label_list = new Map();
-let command_list = [];
-let a= ["     ORG 3000H; 123   ","   MAIN   :   MOV   A   ,01H,   #01H  ;1234  ",   " MOV A  ,#01H  ;  123  ","  CLR C; 位修改  ","   END  ; 12345  "];
 
-console.table(command_list);
+    static insertStr(str, start, newStr){       //在字符串中特定位置插入字符
+        return str.slice(0, start) + newStr + str.slice(start);
+    }
+}
+
+let a= ["     ORG 3000H; 123   ","   MAIN   :   MOV   A   ,01H,   #01H  ;1234  ",   " MOV A  ,#01H  ;  123  ","  CLR C; 位修改  ","   END  ; 12345  "];
