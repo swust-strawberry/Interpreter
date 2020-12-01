aad118beb0731aaba77fefc2740264f1de931008
chengan666 <1156292183@qq.com>
2020-12-01 15:27:19 +0800

    XOR 标准版

--- first/instructionSystem/xor.js
+++ first/instructionSystem/xor.js
@@ -11,31 +11,31 @@ class Xor
         {
             if(k1 === -1)
             {
-                ram.chip.setRegister ( order1.data , SysConvert.to_binary("0"));
+                ram.chip.setRegister ( order1.data , SysConvert.to_hexadecimal("0" +"H"));
                 return ram;
             }
             else
             {
                 let reg = new RegExp("]","[")//"[","]"就不行
                 order1.data.replace(reg,"");//去【】
-                ram.chip.setRegister ( order1.data , SysConvert.to_binary("0"));
+                ram.chip.setRegister ( order1.data , SysConvert.to_hexadecimal("0" + "H"));
                 return ram;
             }
         }
 
         if(k1 ===-1)
-            num1 = SysConvert.to_binary(Addressing_mode.direct( ram.chip.getRamByAddress(order1.data) , ram ) + 'B');
+            num1 = SysConvert.to_binary(Addressing_mode.direct( ram.chip.getRamByAddress(order1.data) , ram ));
         else
-            num1 = SysConvert.to_binary(Addressing_mode.register_indirect( ram.chip.getRamByAddress(order1.data) , ram ) + 'B');
+            num1 = SysConvert.to_binary(Addressing_mode.register_indirect( ram.chip.getRamByAddress(order1.data) , ram ));
         if(k2 ===-1)
-            num2 = SysConvert.to_binary(Addressing_mode.direct( ram.chip.getRamByAddress(order2.data) , ram ) + 'B');
+            num2 = SysConvert.to_binary(Addressing_mode.direct( ram.chip.getRamByAddress(order2.data) , ram ));
         else
-            num2 = SysConvert.to_binary(Addressing_mode.register_indirect( ram.chip.getRamByAddress(order2.data) , ram ) + 'B');
+            num2 = SysConvert.to_binary(Addressing_mode.register_indirect( ram.chip.getRamByAddress(order2.data) , ram ));
 
         for(let i = 0 ; i < num1.length ; i++ )//按位异或
              num1[i] = (parseInt( num1[i] ) ^ parseInt( num2[i] )).toString();//后面是整数，前面是字符串
 
-        ram.chip.setRegister ( order1.data , num1 );
+        ram.chip.setRegister ( order1.data , SysConvert.to_hexadecimal(num1 + 'H'));
         return ram;
     }
 
